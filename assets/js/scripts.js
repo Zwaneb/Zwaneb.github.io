a57a2bd4b6dccc915adaccd550f1e651d484c9a8
ZWANEB <22064@esp.mr>
2024-05-25 12:06:09 +0000

    ajout-point

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,53 @@
-const pdfFiles = [
-    "pdfs/project1.pdf",
-    "pdfs/project2.pdf",
-    "pdfs/project3.pdf"
-];
+document.addEventListener("DOMContentLoaded", function() {
+    const pdfContainer = document.getElementById("pdf-container");
+    const addPdfButton = document.getElementById("add-pdf");
+    const removePdfButton = document.getElementById("remove-pdf");
+
+    // List of initial PDF files
+    const pdfFiles = [
+        "https://zwaneb.github.io/ZwanebprojectsDS/quizCifar-1.pdf",
+        "https://zwaneb.github.io/ZwanebprojectsDS/Titanic%20-%20Stacking%20Exercice.pdf",
+        "https://zwaneb.github.io/ZwanebprojectsDS/zwaneb_ridge.pdf",
+        "https://zwaneb.github.io/ZwanebprojectsDS/Adaboost%20ou%20XGBoost%20.pdf"
+    ];
+
+    function displayPdfLinks() {
+        // Clear existing links
+        pdfContainer.innerHTML = '';
+
+        // Display all PDF links
+        pdfFiles.forEach(function(pdfLink) {
+            const link = document.createElement("a");
+            link.href = pdfLink;
+            link.textContent = pdfLink.split('/').pop().replace(/%20/g, ' '); // Display just the filename
+            link.target = "_blank"; // Open in a new tab
+            pdfContainer.appendChild(link);
+            pdfContainer.appendChild(document.createElement("br"));
+        });
+    }
+
+    function addProjectPdf() {
+        const newPdf = prompt("Enter the URL of the new project PDF:");
+        if (newPdf) {
+            pdfFiles.push(newPdf);
+            displayPdfLinks();
+        }
+    }
+
+    function removeProjectPdf() {
+        const pdfToRemove = prompt("Enter the URL of the project PDF to remove:");
+        const index = pdfFiles.indexOf(pdfToRemove);
+        if (index > -1) {
+            pdfFiles.splice(index, 1);
+            displayPdfLinks();
+        } else {
+            alert("PDF not found.");
+        }
+    }
+
+    addPdfButton.addEventListener("click", addProjectPdf);
+    removePdfButton.addEventListener("click", removeProjectPdf);
+
+    // Display the initial PDF links
+    displayPdfLinks();
+});
